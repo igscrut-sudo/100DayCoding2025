91b1c2d778e24edd914114a8cc09975b97365300
Muh Fauzi <igscrut@gmail.com>
2025-09-09 15:20:48 +0800

    Create day2.java

---
+++ day2.java
@@ -0,0 +1,27 @@
+
+package coding100hari;
+
+import java.io.PrintStream;
+
+
+public class Coding100hari {
+
+   
+    public static void main(String[] args) {
+        System.out.print("Halo");
+        System.out.printf("namaku %s%n","Fauzi","18");
+        System.out.println("namaku Fauzi");
+       }
+}
+   
+        
+        
+        
+          
+        
+    
+        
+    
+        
+        
+
