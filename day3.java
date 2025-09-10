d02e64921c074d7a9b06f3bc3710a212915ec41c
Muh Fauzi <igscrut@gmail.com>
2025-09-10 18:23:47 +0800

    day3.java

---
+++ day3.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+		
+// day 3 coding//
+System.out.println(" Halo nama Saya Muh Fauzi"); // ini adalah komentar//
+
+/*
+ini juga komentar tapi untuk kalimat yang lebih panjang ,bisa untuk lebih dari satu baris 
+*/
+ 
+	}
+}
