5c9e0413205150a200839b5fe65739132a7147f3
Muh Fauzi <igscrut@gmail.com>
2025-09-11 20:41:47 +0800

    day4.java

---
+++ day4.java
@@ -0,0 +1,12 @@
+     
+    public static void main(String[] args) { 
+        /* \t = Memberi Spasi Pada Teks
+           \n = Membuat Baris Baru
+           \? = Menampilkan Tanda Tanya
+        */
+        System.out.println("Nama\t:Muhammad Fauzi Al-Zahmi");
+        System.out.println("Alamat\n: Sapokayyang");
+        System.out.println("Asal sekolah : \'Smkn Labuang\'");
+        System.out.println("Hobi : \"BUlu Tangkis\" ");
+       }
+}
