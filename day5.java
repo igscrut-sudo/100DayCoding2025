9737bcd02244e1441a7c78dd2037da74c83dceb8
Muh Fauzi <igscrut@gmail.com>
2025-09-12 20:46:20 +0800

    day5.java

---
+++ day5.java
@@ -0,0 +1,16 @@
+public class JavaApplication18 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int tinggi = 172;
+        byte berat = 17; 
+        short lb = 15;
+        long tl = 4;
+        System.out.println("Tinggi\t:" + tinggi);
+        System.out.println("Berat\t:" + berat + "kg");
+        System.out.println("Lingkaran Badan\t:"+ lb );
+        System.out.println("tinggi lompatan\t:" + tl);
+        
+    }
+    
+}
