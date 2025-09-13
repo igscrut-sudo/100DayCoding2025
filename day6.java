9246a11af95c91cac588f7b71a617a0af9056a81
Muh Fauzi <igscrut@gmail.com>
2025-09-13 19:28:32 +0800

    day6.java

---
+++ day6.java
@@ -0,0 +1,14 @@
+public class JavaApplication18 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double attack = 2.14;
+        float defense = (float)3.15;
+        
+        System.out.println("Daya Serang Karakter Sekita :" + attack + " physical damage");
+        System.out.println("tingkat pertahanan musuh :"  + defense + "  physical defense ");
+        
+        
+    }
+    
+}
