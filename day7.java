caadd9f8c2f8fdef6b34f8c27678a0956a125521
Muh Fauzi <igscrut@gmail.com>
2025-09-14 20:37:26 +0800

    day7.java

---
+++ day7.java
@@ -0,0 +1,15 @@
+public class JavaApplication18 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+       char inisial  = 'M';
+       boolean isJavaFun = true;
+        System.out.println("Nama Ini Sial :" + inisial);
+        System.out.println("true atau false :"+isJavaFun);
+        
+      
+        
+        
+    }
+    
+}
