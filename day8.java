70f0ec037a88a6866d0a890e4b539fdec8698141
Muh Fauzi <igscrut@gmail.com>
2025-09-15 21:12:56 +0800

    day8.java

---
+++ day8.java
@@ -0,0 +1,17 @@
+public class Coding100hari {
+
+    /**
+     * @param args the command l   ine arguments
+     */
+    public static void main(String[] args) { 
+        /* 
+        tipe data yang digunakan untuk merepresentasikan teks, seperti kata, kalimat, atau simbol, 
+        yang merupakan kumpulan karakter yang tersusun berurutan dan biasanya diapit oleh tanda kutip 
+        tunggal atau ganda
+        */
+        
+        String Perkenalan = " Nama Saya Muh Fauzi, Prodi Informatika, Kelas D";
+        System.out.println("Perkenalkan :" + Perkenalan );
+       }
+}
+   
