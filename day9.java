e2e146a22f998a5294722080735d6c84379fe46a
Muh Fauzi <igscrut@gmail.com>
2025-09-17 18:25:25 +0800

    day9.java

---
+++ day9.java
@@ -0,0 +1,17 @@
+public class Main {
+	public static void main(String[] args) {
+   //sebelum update
+   String nama ;
+   int umur ;
+   nama = "Muh Fauzi";
+   umur =18;
+   System.out.println("Nama awal=" + nama);
+   System.out.println("Umur awal="+ umur);
+   //setelah update
+   nama = "Muh fauzi";
+   umur =19;
+   System.out.println("Nama setelah update="+ nama);
+   System.out.println("Umur setelah update="+ umur);
+
+	}
+}
