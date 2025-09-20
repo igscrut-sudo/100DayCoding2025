381fbc1c83eb836853c13a1570c0328bb4c6bb6e
Muh Fauzi <igscrut@gmail.com>
2025-09-20 17:28:29 +0800

    Soal2.java

---
+++ Soal2.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("nama: muh fauzi");
+		System.out.print("umur: 19\n");
+		System.out.printf("nama%sb, umur%d, IPK%f" ,"muh fauzi",18,3.87);
+		
+	
+	}
+}
