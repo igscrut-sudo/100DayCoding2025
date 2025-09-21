b9789bf9d2d09c3049651ed15c6743a2c75c9078
Muh Fauzi <igscrut@gmail.com>
2025-09-21 13:40:34 +0700

    day14.java

---
+++ day14.java
@@ -0,0 +1,36 @@
+package coding100hari;
+
+
+public class Day14 {
+    
+  public static void main(String[] args){
+ 
+  
+  int intValue = Integer.parseInt("301");
+      System.out.println("int\t:"+intValue);
+      
+  long longValue = Long.parseLong("28");
+      System.out.println("LOng\t:"+ longValue);
+      
+  byte byteValue = Byte.parseByte("22");
+      System.out.println("Byte\t:"+ byteValue);
+      
+  short shortValue = Short.parseShort("23");
+     System.out.println("Short\t:"+ shortValue);
+      
+  boolean boleanValue = Boolean.parseBoolean("False");
+      System.out.println("Boolean\t:"+ boleanValue);
+      
+  double doubleValue = Double.parseDouble("2.33");
+      System.out.println("Double\t:"+doubleValue);
+      
+  float floatValue = Float.parseFloat("2.44");
+      System.out.println("Float\t:"+floatValue);
+      
+  char value = 'S';
+      System.out.println("Chart\t:"+value);
+  
+  
+  }
+    
+}
