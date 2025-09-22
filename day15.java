e587de6a3a488c53fe45214798a1bfeffc049f1d
Muh Fauzi <igscrut@gmail.com>
2025-09-22 21:08:36 +0800

    day15.java

---
+++ day15.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class day15 {
+    
+    public static void main(String [] args){
+        Scanner uji = new Scanner(System.in);
+       
+        int a, b, c;
+        
+        System.out.print("Masukkan Nilai :");
+         a = uji.nextInt();
+         
+        System.out.print("Masukkan NIlai :");
+         b = uji.nextInt();
+         c = a + b ;
+         
+         System.out.println(a+"+"+b+"="+c);
+         
+         c = a-b;
+         System.out.println(a+"-"+b+"="+c);
+        
+        
+        
+                
+             
+        
+    }
+    
+}
