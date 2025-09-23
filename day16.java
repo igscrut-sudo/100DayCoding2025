b2233cc59083f9867aabafd40601bd71ce9b1612
Muh Fauzi <igscrut@gmail.com>
2025-09-23 15:30:58 +0800

    day16.java

---
+++ day16.java
@@ -0,0 +1,27 @@
+
+package coding100hari;
+
+import java.util.Scanner;
+public class day16 {
+    public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    
+    int a,u,e;
+        System.out.print("Masukkan Nilai :");
+         a = input.nextInt();
+        
+        System.out.println("Masukkan Nilai :");
+         u = input.nextInt();
+         
+         e = a*u;
+         System.out.println(a+"x"+u+"="+e);
+         
+         e = a/u;
+         System.out.println(a+":"+u+"="+e);
+        
+        
+        
+       
+    }
+    
+}
