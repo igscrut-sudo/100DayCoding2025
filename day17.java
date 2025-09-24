2e8e861f30e31c4cb8d3f0158863b12097a8b17c
Muh Fauzi <igscrut@gmail.com>
2025-09-24 18:00:21 +0800

    day17.java

---
+++ day17.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class day17 {
+    
+    public static void main (String [] args){
+    Scanner input = new Scanner (System.in);
+        System.out.print("Masukkan Nilia :");
+        int a = input.nextInt();
+        System.out.print("Masukkan Nilia :");
+        int b = input.nextInt();
+        
+        int e = a%b;
+         
+      System.out.println("sisa bagi :"+e);
+    
+    
+    }
+}
