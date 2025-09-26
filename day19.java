299ae019522d3d41738bfa23a4dd8ad5c7147e81
Muh Fauzi <igscrut@gmail.com>
2025-09-26 18:03:52 +0700

    day19.java

---
+++ day19.java
@@ -0,0 +1,14 @@
+public class day19 {
+    
+    public static void main(String [] args){
+    
+        long besar =200777777;
+        int kecil =(int)besar;
+        
+        System.out.print(kecil);
+        
+        
+    
+    }
+    
+}
