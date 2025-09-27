22ce1064d9832158606ceb60b81d3c7fd6241374
Muh Fauzi <igscrut@gmail.com>
2025-09-27 19:10:38 +0700

    day20.java

---
+++ day20.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package coding100hari;
+
+
+public class day20 {
+    public static void main(String[] args) {
+        
+        int h = 2243;
+        byte a = 12;
+        long b = 13;
+        short c = 25;
+        double d = 22;
+        char e = 'A';
+        float f =(float) 267;
+        boolean g = true;
+        
+        System.out.println("=== Mengubah Tipe Data Primitif ke String ===");
+        
+        String str = String.valueOf("int\t:"+h);
+        System.out.println(str);
+        
+        String str1 = String.valueOf("byte\t:"+a);
+        System.out.println(str1);
+        
+        String str2 = String.valueOf("short\t:"+b);
+        System.out.println(str2);
+        
+        String str3 = String.valueOf("short\t:"+c);
+        System.out.println(str3);
+        
+        String str4 = String.valueOf("double\t:"+d);
+        System.out.println(str4);
+        
+        String str5 = String.valueOf("char\t:"+e);
+        System.out.println(str5);
+        
+        String str6 = String.valueOf("float\t:"+f);
+        System.out.println(str6);
+        
+        String str51 = String.valueOf("Boolean\t:"+g);
+        System.out.println(str51);
+        
+    }
+    
+}
