5e5963613670df6fd815bf3baa14b814983cc820
Muh Fauzi <igscrut@gmail.com>
2025-09-28 21:44:58 +0800

    day21.java

---
+++ day21.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		int f = 6;
+int z = 11;
+
+System.out.println("Sebelum ditukar : a = " + a + ", b = " + b);
+
+// Langkah-langkah penukaran
+f = f + z; // f sekarang 17(6 + 11)
+z = f - z; // z sekarang 5(6 - 11)
+
+System.out.println("ditukar : f = " + f + ", z = " + z);  
+	}
+}
