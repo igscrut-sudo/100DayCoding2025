9ea6e7f8bf9cb3af796ffa0c34569abd2293e831
Muh Fauzi <igscrut@gmail.com>
2025-09-29 21:21:40 +0800

    day22.java

---
+++ day22.java
@@ -0,0 +1,23 @@
+
+public class day21 {
+    
+    public static void main(String[] args) {
+        Scanner lp = new Scanner (System.in);
+        /*
+        Menghitung luas Persegi
+        
+        L = s*s;
+        */
+        
+        int l,s;
+        
+        System.out.print("Hitung luas Persegi :");
+        s = lp.nextInt();
+        
+        l = s*s;
+        
+        System.out.println("Luas Persegi:"+l);
+        
+    }
+    
+}
