2fc4f44a23ba2954367d103c920949bbda6f6699
Muh Fauzi <igscrut@gmail.com>
2025-09-30 22:06:39 +0800

    day23.java

---
+++ day23.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class day23 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+        int p,l,i;
+        System.out.print("Masukkan Nilai :");
+        p = sc.nextInt();
+        System.out.print("Masukkan Nilai :");
+        i = sc.nextInt();
+        
+        
+        l = p * i;
+        
+        System.out.println("Luas Persegi Panjang :"+l);
+        
+        
+    }
+    
+}
