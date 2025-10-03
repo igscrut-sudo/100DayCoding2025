ecb466864b32f868830b1db365e7d4ed01eff5fb
Muh Fauzi <igscrut@gmail.com>
2025-10-03 20:30:36 +0800

    day26.java

---
+++ day26.java
@@ -0,0 +1,23 @@
+package coding100hari;
+
+public class day26 {
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 10;
+        int c = 10;
+        int d = 10;
+        
+        a += 2;
+        b -= 6;
+        c /= 5;
+        d %= 2;
+        
+        System.out.println("Nilai a +=\t:"+a);
+        System.out.println("Nilai b-=\t:"+b);
+        System.out.println("Nilai c/=\t:"+c);
+        System.out.println("Nilai d%=\t:"+d);
+        
+    }
+    
+}
