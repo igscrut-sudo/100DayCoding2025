d5dec963a9e776192e18291ff7d35e338234b3a9
Muh Fauzi <igscrut@gmail.com>
2025-10-06 21:29:06 +0800

    day29.java

---
+++ day29.java
@@ -0,0 +1,11 @@
+public class day29 {
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 10;
+        
+        System.out.println( a < b);
+        System.out.println(a > b);
+    }
+    
+}
