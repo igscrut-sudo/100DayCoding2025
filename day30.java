ef8827fe1d86e935587bd6e1e62ab109d4f661e8
Muh Fauzi <igscrut@gmail.com>
2025-10-07 20:47:12 +0800

    day30.java

---
+++ day30.java
@@ -0,0 +1,13 @@
+
+package coding100hari;
+
+public class day30 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 12;
+        
+        System.out.println( a <= b);
+        System.out.println( a >= b);
+    }
+    
+}
