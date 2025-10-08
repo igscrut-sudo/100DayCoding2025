9e093cb7f6b53889e1e83bb00125c5d174560105
Muh Fauzi <igscrut@gmail.com>
2025-10-08 20:33:24 +0800

    day31.java

---
+++ day31.java
@@ -0,0 +1,10 @@
+public class day31 {
+    public static void main(String[] args) {
+        
+        boolean a = true;
+        boolean b = true;
+        
+        System.out.println(a && b);
+    }
+    
+}
