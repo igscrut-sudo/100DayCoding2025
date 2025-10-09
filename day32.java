7e9ce64d36f0165957c54ffee7d08080e33829bb
Muh Fauzi <igscrut@gmail.com>
2025-10-09 20:51:24 +0800

    day32.java

--- day32.java
+++ day32.java
@@ -1,9 +1,9 @@
 public class day32 {
     public static void main(String[] args) {
        boolean a = true;
-       boolean b = false;
+       boolean c = false;
        
-        System.out.println( a || b);
+        System.out.println( a || c);
         
         
         
