c5d2e1946e22ee7536b803f10d98da5d01319604
Muh Fauzi <igscrut@gmail.com>
2025-10-09 20:50:33 +0800

    Create day32.java

---
+++ day32.java
@@ -0,0 +1,15 @@
+public class day32 {
+    public static void main(String[] args) {
+       boolean a = true;
+       boolean b = false;
+       
+        System.out.println( a || b);
+        
+        
+        
+        
+    
+  
+    
+    }
+}
