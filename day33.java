cf8df472b7ad0de6d423782d7eed30c4e3b56db6
Muh Fauzi <igscrut@gmail.com>
2025-10-10 19:49:47 +0700

    day33.java

---
+++ day33.java
@@ -0,0 +1,10 @@
+public class day33 {
+    public static void main(String[] args) {
+        
+                 int a = 12;
+                 int b = 23;
+                 
+                 System.out.println( a != b);
+    }
+    
+}
